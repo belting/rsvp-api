ba9e3db2fd0bba0ebafd5ede65b7a27f808ea422
Brian Elting <belting01@gmail.com>
2017-09-18 23:19:16 -0700

    Added properties to party

--- models/party.js
+++ models/party.js
@@ -4,6 +4,13 @@ const Schema = mongoose.Schema;
 const Party = new Schema({
     responseType: String,
     responseAt: Date,
+    name: String,
+    addressTo: String,
+    addressLineOne: String,
+    addressLineTwo: String,
+    city: String,
+    state: String,
+    zip: String,
     guests: [{
         type: Schema.ObjectId,
         ref: 'Guest'
